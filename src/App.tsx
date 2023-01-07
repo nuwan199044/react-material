8ed603d0447e2df4cc23aec12c43fe921e1e305a
Nuwan <nuwan199044@gmail.com>
2023-01-07 20:11:49 +0530

    Usage of Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { MuiCheckbox } from './components/MuiCheckbox';
+import { MuiSwitch } from './components/MuiSwitch';
+// import { MuiCheckbox } from './components/MuiCheckbox';
 // import { MuiRadioButton } from './components/MuiRadioButton';
 // import { MuiSelect } from './components/MuiSelect';
 // import { MuiTextField } from './components/MuiTextField';
@@ -18,7 +19,8 @@ function App() {
       {/* <MuiTextField /> */}
       {/* <MuiSelect /> */}
       {/* <MuiRadioButton /> */}
-      <MuiCheckbox />
+      {/* <MuiCheckbox /> */}
+      <MuiSwitch />
     </div>
   );
 }
