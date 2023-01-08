d90f761f67a50467ec88cc1c872aa9851ed09ed7
Nuwan <nuwan199044@gmail.com>
2023-01-08 13:27:38 +0530

    Usage of Stack

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,31 +1,93 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Divider } from "@mui/material";
 
 export const MuiLayout = () => {
   return (
-    <>
-      <Box component="span">MuiLayout</Box>
-      <Box
-        sx={{
-          width: "100px",
-          height: "100px",
-          padding: "16px",
-          bgcolor: "primary.main",
-          color: "white",
-          "&:hover": {
-            bgcolor: "primary.light",
-          },
-        }}
+    <Stack spacing={2}>
+      <Stack spacing={0} direction="row" sx={{ border: "1px solid" }}>
+        <Box
+          sx={{
+            width: "50px",
+            height: "50px",
+            padding: "8px",
+            bgcolor: "primary.main",
+            color: "white",
+            "&:hover": {
+              bgcolor: "primary.light",
+            },
+          }}
+        >
+          Box 1
+        </Box>
+        <Box width={50} height={50} p={1} bgcolor="success.light">
+          Box 2
+        </Box>
+      </Stack>
+      <Stack
+        spacing={2}
+        direction="column"
+        sx={{ border: "1px solid" }}
+        divider={<Divider orientation="horizontal" flexItem />}
       >
-        MuiLayout
-      </Box>
-      <Box
-        width={100}
-        height={100}
-        p={2}
-        bgcolor='success.light'
+        <Box
+          sx={{
+            width: "50px",
+            height: "50px",
+            padding: "8px",
+            bgcolor: "primary.main",
+            color: "white",
+            "&:hover": {
+              bgcolor: "primary.light",
+            },
+          }}
+        >
+          Box 1
+        </Box>
+        <Box width={50} height={50} p={1} bgcolor="success.light">
+          Box 2
+        </Box>
+      </Stack>
+      <Stack spacing={0} direction="row-reverse" sx={{ border: "1px solid" }}>
+        <Box
+          sx={{
+            width: "50px",
+            height: "50px",
+            padding: "8px",
+            bgcolor: "primary.main",
+            color: "white",
+            "&:hover": {
+              bgcolor: "primary.light",
+            },
+          }}
+        >
+          Box 1
+        </Box>
+        <Box width={50} height={50} p={1} bgcolor="success.light">
+          Box 2
+        </Box>
+      </Stack>
+      <Stack
+        spacing={0}
+        direction="column-reverse"
+        sx={{ border: "1px solid" }}
       >
-        MuiLayout
-      </Box>
-    </>
+        <Box
+          sx={{
+            width: "50px",
+            height: "50px",
+            padding: "8px",
+            bgcolor: "primary.main",
+            color: "white",
+            "&:hover": {
+              bgcolor: "primary.light",
+            },
+          }}
+        >
+          Box 1
+        </Box>
+        <Box width={50} height={50} p={1} bgcolor="success.light">
+          Box 2
+        </Box>
+      </Stack>
+    </Stack>
   );
 };
